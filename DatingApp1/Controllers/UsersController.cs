3f907bbafa9f82fbbb73955495af4b54979f416f
Ismail Cherif <cherif.ismail.24@gmail.com>
2023-08-22 21:21:02 +0100

    End of section 2

---
+++ DatingApp1/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DatingApp1.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using DatingApp1.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp1.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+		{
+           _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+
+            
+        }
+
+        // api/user/"user id"
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+           return await _context.Users.FindAsync(id);
+        }
+    }
+}
+
---
+++ DatingApp1/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using DatingApp1.Entities;
+namespace DatingApp1.Data
+{
+	public class DataContext : DbContext
+	{
+		
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
+
